8568d7527b109d8d1bd5c61a4326b19a5ab0c704
jjhhh2266 <geehkim@gmail.com>
2017-08-19 14:16:04 +0900

    Initial commit

---
+++ app/src/main/java/com/bridou_n/beaconscanner/dagger/ActivityComponent.java
@@ -0,0 +1,20 @@
+package com.bridou_n.beaconscanner.dagger;
+
+import android.app.Activity;
+
+import com.bridou_n.beaconscanner.features.beaconList.MainActivity;
+
+import dagger.Component;
+
+/**
+ * Created by bridou_n on 08/10/2016.
+ */
+@PerActivity
+@Component(dependencies = AppComponent.class,
+        modules = {
+                BluetoothModule.class,
+                AnimationModule.class
+        })
+public interface ActivityComponent {
+        void inject(Activity activity);
+}
